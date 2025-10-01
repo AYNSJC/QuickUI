5205e1f1b44d7c3d3aff92b410b3c1f0b221c8e6
AYNSJC <chandhokaarya@gmail.com>
2025-10-01 22:28:48 +0530

    Update Example.py

--- src/Example.py
+++ src/Example.py
@@ -1,5 +1,6 @@
 import QuickUI
 
+# Logic -- Can be ignored --
 def check_age(age: int) -> str:
     if not agreed.get():
         return "Agree to T&C"
@@ -9,9 +10,20 @@ def check_age(age: int) -> str:
     else:
         return "Can't drive"
 
+# Creates window
 QuickUI.create_window(user_padding=10, title="QuickUI - drive test")
+
+# Creates text
 QuickUI.create_label(info="Can you drive?", y=0)
+
+# Takes Input
 user_age = QuickUI.create_entry(default="Age", y=1)
+
+# Takes Checkbox Input
 agreed = QuickUI.create_checkbox(info="Agree to T&C", y=2)
+
+# Creates button
 QuickUI.create_button(info="Check", y=3, command=lambda: QuickUI.show_message("QuickUI", check_age(age=int(user_age.get()))))
+
+# Refresh UI screen
 QuickUI.refresh_ui()
