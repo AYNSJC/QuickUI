62d9503f52af08024dcec1c5e002f89dbc9f9837
AYNSJC <chandhokaarya@gmail.com>
2025-10-01 20:12:23 +0530

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,6 +1,12 @@
 import QuickUI
 
+agreed = None
+
 def check_age(age: int) -> str:
+    global agreed
+    if not agreed.get():
+        return "Agree to T&C"
+
     if age >= 18:
         return "Can drive"
     else:
@@ -10,5 +16,6 @@ def check_age(age: int) -> str:
 QuickUI.create_window(user_padding=10, title="QuickUI - drive test")
 QuickUI.create_label(info="Can you drive?", y=0)
 user_age = QuickUI.create_entry(default="Age", y=1)
-QuickUI.create_button(info="Check", y=2, command=lambda: QuickUI.show_message("QuickUI", check_age(age=int(user_age.get()))))
+agreed = QuickUI.create_checkbox(info="Agree to T&C", y=2)
+QuickUI.create_button(info="Check", y=3, command=lambda: QuickUI.show_message("QuickUI", check_age(age=int(user_age.get()))))
 QuickUI.refresh_ui()
